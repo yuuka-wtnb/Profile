803afd6e6fa4cc458555a6ae0cae6306f53d9d7d
Yuka Watanabe <yuuka.wtnb.011@gmail.com>
2020-08-31 22:30:46 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by Yuuka Watanabe on 2020/08/31.
+//  Copyright © 2020 Yuka Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
